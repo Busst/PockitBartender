7f9865f07c7f6634c6ab693f0b70a3ad93684b69
Busst <Tbuss05@gmail.com>
2019-04-21 23:41:03 -0400

    added direction tag to show drinks

--- App/bartender/src/views/ShowDrink.vue
+++ App/bartender/src/views/ShowDrink.vue
@@ -4,11 +4,13 @@
         <template v-if="isEditing">
             <h1>{{ drinkName }}</h1>
             <textarea v-model="drinkRecipe" placeholder="Drink Recipe"></textarea><br/>
+            <textarea v-model="drinkDirections" placeholder="Drink Directions"></textarea><br/>
             <button @click="handleSaveDrink">Concoct Drink!</button>
         </template>
         <template v-else>
             <h1>{{ drinkName }}</h1>
             <p>{{ drinkRecipe }}</p>
+            <p>{{ drinkDirections }}</p>
             <button @click="handleEdit">Update Recipe</button>
         </template>
     </div>
@@ -23,6 +25,7 @@
       return {
         drinkName: '',
         drinkRecipe: '',
+        drinkDirections: '',
         isEditing: false
       }
     },
@@ -37,6 +40,7 @@
                     const drink = response.data
                     that.drinkName = drink.name
                     that.drinkRecipe = drink.recipe
+                    that.drinkDirections = drink.directions
                 })
                 .catch((error) => {
                     console.log(error)
@@ -51,7 +55,8 @@
             const that = this
             const drink = {
                 name: this.drinkName,
-                recipe: this.drinkRecipe
+                recipe: this.drinkRecipe,
+                directions: this.drinkDirections
             }
             Services.saveDrink(drink)
             .then((response) => {
