84179f80fc6dcb2caa43c879d28d9eecc0e753ba
Busst <Tbuss05@gmail.com>
2019-06-16 14:34:12 -0400

    home screen upvotes

--- App/bartender/src/services/index.js
+++ App/bartender/src/services/index.js
@@ -8,5 +8,6 @@ export default {
   getUser: id => axios.get(API_BASE + '/users/' + id),
   saveDrink: drink => axios.post(API_BASE + '/drinks', drink),
   getDrink: name => axios.get(API_BASE + '/drinks/' + name),
-  filterDrinks: searchObject => axios.post(API_BASE + "/drinks/filter", searchObject)
+  filterDrinks: searchObject => axios.post(API_BASE + "/drinks/filter", searchObject),
+  upVoteDrink: drink => axios.post(API_BASE + '/drinks/upvote', drink)
 }
--- Server/PockitBartender/routes/drink.js
+++ Server/PockitBartender/routes/drink.js
@@ -35,16 +35,19 @@ router.post('/', function (req, res) {
     var name = req.body.name;
     var recipe = req.body.recipe;
     var directions = req.body.directions;
-
+    var rating = 0;
     var drinkCol = db.collection('drinks');
 
     drinkCol.doc(name).set({
         name,
         recipe,
-        directions
+        directions,
+        rating
+
+
     });
-    console.log({ name, recipe, directions })
-    res.send({ name, recipe, directions });
+    console.log({ name, recipe, directions, rating })
+    res.send({ name, recipe, directions, rating });
 
 });
 
@@ -58,7 +61,12 @@ router.post('/filter', function (req, res, next) {
             snapshot.forEach(doc => {
                 var key = doc.id.toLowerCase();
                 if (key.indexOf(searchTerm) >= 0) {
+                    
                     foundDrinks.push(doc.data())
+                    
+                    if (foundDrinks[foundDrinks.length-1].rating === undefined){
+                        foundDrinks[foundDrinks.length-1].rating = 0;
+                    }
                 }
             });
             res.send(foundDrinks);
@@ -68,6 +76,125 @@ router.post('/filter', function (req, res, next) {
 
         });
 });
+
+router.post('/upvote', function (req, res, next ) {
+    var name = req.body.name;
+    var deltaRating = req.body.rating;
+    var id = req.body.user;
+    var voteDir = req.body.vote;
+    var drinkCol = db.collection('drinks');
+    var userCol = db.collection('users');
+    drinkCol.doc(name).get()
+        .then(doc => {
+            if (!doc.exists) {
+                console.log('Does not exist');
+            } else {
+                var directions = doc.data().directions;
+                var recipe = doc.data().recipe;
+                var rating;
+                if (doc.data().rating === undefined || doc.data().rating === NaN) {
+                    rating = 0;
+                } else {
+                    rating = doc.data().rating;
+                }
+                rating += deltaRating;
+                drinkCol.doc(name).set({
+                    name,
+                    recipe,
+                    directions,
+                    rating
+                });
+                console.log({ name, recipe, directions, rating })
+                console.log();
+                res.send({ name, recipe, directions, rating });
+            }
+        })
+        .catch(err => {
+
+        });
+    userCol.doc(id).get()
+        .then(user => {
+            console.log(user.data());
+            var upvotedDrinks = user.data().upvotedDrinks;
+            var downvotedDrinks = user.data().downvotedDrinks;
+            if (upvotedDrinks === undefined){
+                upvotedDrinks = "";
+            }
+            var downvotedDrinks = user.data().downvotedDrinks;
+            if (downvotedDrinks === undefined){
+                downvotedDrinks = "";
+            }
+            
+            
+            /*  
+                
+            
+                
+                if up but 2 then remove from downvoted add to up
+
+                if down but -2 then remove from upvoted add to down
+               
+
+            */
+
+            if (deltaRating > 0){
+                if (voteDir === "up"){
+                    if (upvotedDrinks.length === 0){
+                        upvotedDrinks += name;
+                    } else {
+                        upvotedDrinks += "::" + name;
+                    }
+                } 
+                if (deltaRating != "up" || deltaRating > 1) {
+                    if (downvotedDrinks.length != 0){
+                        downvotedDrinks = removeFromString(name, downvotedDrinks);
+                    } 
+                }
+            } else {
+                if (voteDir != "up") {
+                    if (downvotedDrinks.length === 0){
+                        downvotedDrinks += name;
+                    } else {
+                        downvotedDrinks += "::" + name;
+                    }
+                }
+
+                if (voteDir === "up" || deltaRating < -1){
+                    if (upvotedDrinks.length != 0){
+                        upvotedDrinks = removeFromString(name, upvotedDrinks);
+                    } 
+                } 
+                
+            } 
+            
+            var email = user.data().email;
+            var userName = user.data().userName;
+            
+            userCol.doc(id).set({
+                id,
+                email,
+                userName,
+                upvotedDrinks,
+                downvotedDrinks
+            });
+            
+        })
+        .catch(err => {
+            console.log(err);
+        });
+    
+});
+
+function removeFromString(name, str) {
+    var strArray = str.split("::");  
+    for (var i = 0; i < strArray.length; i++){
+        if (strArray[i] === name){
+            strArray.splice(i, 1);
+             str = strArray.join("::");
+        }
+    }
+    return str;
+}
 
 
 
